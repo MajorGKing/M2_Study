3f8b42be91ba3bdd572fef45b2922fa9658e8ed6
DESKTOP-Q8290FB\Major <majorgking@gmail.com>
2025-03-04 23:31:07 +0900

    서버 퀘스트 추가 등으로 인해 바뀐 DB업데이트

---
+++ Server/GameServer/Migrations/20250304124759_add20250304.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class add20250304 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnchantCount",
+                table: "Item");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PosY",
+                table: "Hero",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PosX",
+                table: "Hero",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateTable(
+                name: "Quest",
+                columns: table => new
+                {
+                    OwnerDbId = table.Column<int>(type: "int", nullable: false),
+                    TemplateId = table.Column<int>(type: "int", nullable: false),
+                    State = table.Column<int>(type: "int", nullable: false),
+                    QuestTasks = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quest", x => new { x.OwnerDbId, x.TemplateId });
+                    table.ForeignKey(
+                        name: "FK_Quest_Hero_OwnerDbId",
+                        column: x => x.OwnerDbId,
+                        principalTable: "Hero",
+                        principalColumn: "HeroDbId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quest");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EnchantCount",
+                table: "Item",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PosY",
+                table: "Hero",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PosX",
+                table: "Hero",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
